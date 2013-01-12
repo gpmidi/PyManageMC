c568f4fa410258bfab687d476b0ea576daeccb44
Paulson McIntyre <paul@gpmidi.net>
2013-01-11 21:22:45 -0500

    More model progress

--- manageMC/extern/models.py
+++ manageMC/extern/models.py
@@ -20,9 +20,120 @@ from django.contrib.auth.models import User
 from extern.validators import validateHostIP
 from django.core.validators import MinLengthValidator, MaxValueValidator, MinValueValidator
 
-class UserProfile(models.Model):
-    user = models.OneToOneField(User)
+class ExtraUserEmail(models.Model):
+    profile = models.ForeignKey(
+                                'UserProfile',
+                                null = False,
+                                )
+    EMAIL_TYPES = (
+                   (0, "Other"),
+                   (1, "Personal"),
+                   (2, "Work"),
+                   (3, "ER Only"),
+                   (4, "Automated Notifications"),
+                   )
+    emailType = models.SmallIntegerField(
+                                         null = False,
+                                         default = "Other",
+                                         choices = EMAIL_TYPES,
+                                         verbose_name = "Email Use",
+                                         )
+    email = models.EmailField(
+                              null = False,
+                              verbose_name = "Email Address",
+                              )
+    private = models.BooleanField(
+                                null = False,
+                                default = True,
+                                verbose_name = "Private",
+                                help_text = "Do not allow non-superusers to see this address. ",
+                                )
     
+class MinecraftUsername(models.Model):
+    """ One of a minecraft user's in-game usernames """
+    profile = models.ForeignKey(
+                                'UserProfile',
+                                null = False,
+                                )
+    username = models.EmailField(
+                                 null = False,
+                                 verbose_name = "Minecraft Username",
+                                 max_length = 255,
+                                 )
+    private = models.BooleanField(
+                                null = False,
+                                default = True,
+                                verbose_name = "Private",
+                                help_text = "Do not allow non-superusers to see this username",
+                                )
+    verified = models.BooleanField(
+                                   null = False,
+                                   default = False,
+                                   verbose_name = "Verified",
+                                   help_text = "Has this address been verified",
+                                   )
+
+
+class UserPhoneNumber(models.Model):
+    profile = models.ForeignKey(
+                                'UserProfile',
+                                null = False,
+                                )
+    PHONE_TYPES = (
+                   (0, "Personal Cell"),
+                   (1, "Home Land Line"),
+                   (2, "Work Cell"),
+                   (3, "ER Only"),
+                   (4, "Work Land Line"),
+                   )
+    phoneType = models.SmallIntegerField(
+                                         null = False,
+                                         default = "Personal Cell",
+                                         choices = PHONE_TYPES,
+                                         verbose_name = "Phone number Type",
+                                         )
+    phoneNumber = models.CharField(
+                                   null = False,
+                                   blank = False,
+                                   verbose_name = "Phone Number",
+                                   help_text = "The phone number, including area code and country code",
+                                   )
+    private = models.BooleanField(
+                                null = False,
+                                default = True,
+                                verbose_name = "Private",
+                                help_text = "Do not allow non-superusers to see this phone number",
+                                )
+    
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(
+                                User,
+                                null = False,
+                                verbose_name = "User",
+                                )
+    
+    screenname = models.SlugField(
+                            primary_key = True,
+                            null = False,
+                            blank = False,
+                            max_length = 255,
+                            db_index = True,
+                            verbose_name = "Screen Name",
+                            help_text = "The user's screenname. May only included letters, numbers, underscores, and hyphens. ",
+                            )
+    publicName = models.CharField(
+                                  null = True,
+                                  blank = True,
+                                  max_length = 255,
+                                  verbose_name = "The user's publicly visible name. Optional. ",
+                                  )
+    miscContactInfo = models.TextField(
+                                       null = True,
+                                       blank = True,
+                                       max_length = 65536,
+                                       verbose_name = "Miscellaneous Contact Info",
+                                       )
 
 class ServerInstanceExternalInfo(models.Model):
     """ Information to access a server - ie hostname/IP and port
@@ -34,7 +145,7 @@ class ServerInstanceExternalInfo(models.Model):
                             max_length = 255,
                             db_index = True,
                             verbose_name = "Access Name",
-                            help_text = "A short, computer friendly name for the access info. My only included letters, numbers, underscores, and hyphens. ",
+                            help_text = "A short, computer friendly name for the access info. May only included letters, numbers, underscores, and hyphens. ",
                             )
     host = models.CharField(
                             null = False,
@@ -63,6 +174,53 @@ class ServerInstanceExternalInfo(models.Model):
                                  verbose_name = "Server Instance",
                                  help_text = "The server instance that this access is for",
                                  )
+
+
+class ServerSystem(models.Model):
+    """ A physical or virtual system that one or more Minecraft servers run on """
+    name = models.SlugField(
+                            primary_key = True,
+                            null = False,
+                            blank = False,
+                            max_length = 255,
+                            db_index = True,
+                            verbose_name = "System Name",
+                            help_text = "A short, computer friendly name for this system/server/box. May only included letters, numbers, underscores, and hyphens. ",
+                            )
+    admins = models.ManyToManyField(
+                                    User,
+                                    null = False,
+                                    verbose_name = "Admins",
+                                    help_text = "Users who have administrative access to this server",
+                                    )
+    owner = models.ForeignKey(
+                              User,
+                              null = False,
+                              blank = False,
+                              verbose_name = "Owner",
+                              help_text = "The user that is ultimately responsible for this server",
+                              )
+    
+
+class ServerSystemIPs(models.Model):
+    """ An internal IP address of a physical or virtual system that one or more Minecraft servers run on """
+    name = models.SlugField(
+                            primary_key = True,
+                            null = False,
+                            blank = False,
+                            max_length = 255,
+                            db_index = True,
+                            verbose_name = "System IP Name",
+                            help_text = "A short, computer friendly name for this IP. May only included letters, numbers, underscores, and hyphens. ",
+                            )
+    ip = models.IPAddressField(
+                               null = False,
+                               verbose_name = "IP",
+                               )
+    system = models.ForeignKey(
+                               ServerSystem,
+                               verbose_name = "System",
+                               )
     
 class ServerInstance(models.Model):
     name = models.SlugField(
@@ -72,7 +230,7 @@ class ServerInstance(models.Model):
                             max_length = 255,
                             db_index = True,
                             verbose_name = "Instance Name",
-                            help_text = "A short, computer friendly name for the server instance. My only included letters, numbers, underscores, and hyphens. ",
+                            help_text = "A short, computer friendly name for the server instance. May only included letters, numbers, underscores, and hyphens. ",
                             )
     admins = models.ManyToManyField(
                                     User,
@@ -85,19 +243,21 @@ class ServerInstance(models.Model):
                               null = False,
                               blank = False,
                               verbose_name = "Owner",
-                              help_text = "The user that is ultermatly responsable for this server instance",
+                              help_text = "The user that is ultimately responsible for this server instance",
                               )
     
     SERVER_STATUS = (
                        ('Active', (# 100-199
                             (100, "Active"),
                             (101, "On-Demand"),
+                            (102, "On-Hold"),
                             )
                         ),
                      ('Inactive', (# 0-99
                             (0, "Created"),
                             (1, "Archived"),
                             (2, "Unknown"),
+                            (3, "Deleted"),
                             )
                         ),
                    )
@@ -122,5 +282,24 @@ class ServerInstance(models.Model):
                                    verbose_name = "Description",
                                    help_text = "A description and other useful info",
                                    )
-    
-    
+    system = models.ForeignKey(
+                               ServerSystem,
+                               null = False,
+                               verbose_name = "System",
+                               help_text = "The server/system/box/etc that this Minecraft server instance runs on",
+                               )
+    internalIP = models.IPAddressField(
+                               null = False,
+                               verbose_name = "Internal IP",
+                               help_text = "The internal IP address of the server",
+                               )
+    port = models.IntegerField(
+                               null = False,
+                               default = 25565,
+                               verbose_name = "Port",
+                               help_text = "The TCP port that the server listens on",
+                               validators = [
+                                             MinValueValidator(1),
+                                             MaxValueValidator(65535),
+                                             ],
+                               )
