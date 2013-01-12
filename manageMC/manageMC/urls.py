87e97d5bc488e8db09a31c8fe53690b33203ce35
Paulson McIntyre <paul@gpmidi.net>
2013-01-12 10:13:01 -0500

    Removed pointless comment

--- manageMC/manageMC/urls.py
+++ manageMC/manageMC/urls.py
@@ -16,7 +16,6 @@
 #===============================================================================
 from django.conf.urls import patterns, include, url
 
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
