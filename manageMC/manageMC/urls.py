6edf43c1576971ab53ac5fb6a441f4cef61c9982
Paulson McIntyre <paul@gpmidi.net>
2014-03-30 00:24:57 -0400

    Fixing 1.4->1.7

--- manageMC/manageMC/urls.py
+++ manageMC/manageMC/urls.py
@@ -36,19 +36,22 @@ urlpatterns = patterns('',
     # User
     url(r'^accounts/login(?:/)?$', 'django.contrib.auth.views.login',),
     url(r'^accounts/profile(?:/)?$', 'extern.views.userEditProfile', name = "UserProfileEdit"),
-    url(r'^accounts/password/change(?:/)?$', 'django.contrib.auth.views.password_change', name = "UserChangePassword"),
-    url(r'^accounts/password/done(?:/)?$', 'django.contrib.auth.views.password_change_done', name = "UserChangePasswordDone"),
-    url(r'^accounts/logout(?:/)?$', 'django.contrib.auth.views.logout_then_login', name = "UserLogout"),
-    url(r'^accounts/reset(?:/)?$', 'django.contrib.auth.views.password_reset', name = "UserResetPassword"),
-    url(r'^accounts/reset/done(?:/)?$', 'django.contrib.auth.views.password_reset_done'),
-    url(r'^accounts/reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)(?:/)?$', 'django.contrib.auth.views.password_reset_confirm'),
-    url(r'^accounts/reset/done(?:/)?$', 'django.contrib.auth.views.password_reset_complete'),
-    
+    url(r'^accounts/password/change(?:/)?$', 'django.contrib.auth.views.password_change', name = "password_change"),
+    url(r'^accounts/password/done(?:/)?$', 'django.contrib.auth.views.password_change_done', name = "password_change_done"),
+    url(r'^accounts/logout(?:/)?$', 'django.contrib.auth.views.logout_then_login', name = "logout_then_login"),
+    url(r'^accounts/reset(?:/)?$', 'django.contrib.auth.views.password_reset', name = "password_reset"),
+    url(r'^accounts/reset/done(?:/)?$', 'django.contrib.auth.views.password_reset_done', name = 'password_reset_done'),
+    url(r'^accounts/reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)(?:/)?$', 'django.contrib.auth.views.password_reset_confirm', name = 'password_reset_confirm'),
+    url(r'^accounts/reset/done(?:/)?$', 'django.contrib.auth.views.password_reset_complete', name = 'password_reset_complete'),
+
     # Server access
     url(r'^servers(?:/)?$', 'minecraft.views.servers.index'),
     url(r'^servers/new(?:/)?$', 'minecraft.views.servers.newserver'),
     url(r'^servers/(\d+)(?:/)?$', 'minecraft.views.servers.view'),
     
+    # Auth
+    url(r'', include('registration.backends.default.urls')),
+
     # Admin
     url(r'^admin/', include(admin.site.urls)),
 )
